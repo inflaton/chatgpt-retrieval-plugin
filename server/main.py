6a86351d628154cdcb49b26e2ffc3fd98b241566
Donghao Huang <donghao.huang@mastercard.com>
2023-04-02 12:44:33 +0800

    recover code from upstream

--- server/main.py
+++ server/main.py
@@ -3,6 +3,7 @@ import uvicorn
 from fastapi import FastAPI, File, HTTPException, Depends, Body, UploadFile
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.staticfiles import StaticFiles
+import config
 
 from models.api import (
     DeleteRequest,
@@ -15,20 +16,6 @@ from models.api import (
 from datastore.factory import get_datastore
 from services.file import get_document_from_file
 
-import config
-
-app = FastAPI()
-app.mount("/.well-known", StaticFiles(directory=".well-known"), name="static")
-
-# Create a sub-application, in order to access just the query endpoint in an OpenAPI schema, found at http://0.0.0.0:8000/sub/openapi.json when the app is running locally
-sub_app = FastAPI(
-    title="Retrieval Plugin API",
-    description="A retrieval API for querying and filtering documents based on natural language queries and metadata",
-    version="1.0.0",
-    servers=[{"url": "https://your-app-url.com"}],
-)
-app.mount("/sub", sub_app)
-
 bearer_scheme = HTTPBearer()
 BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
 assert BEARER_TOKEN is not None
@@ -40,13 +27,26 @@ def validate_token(credentials: HTTPAuthorizationCredentials = Depends(bearer_sc
     return credentials
 
 
+app = FastAPI(dependencies=[Depends(validate_token)])
+app.mount("/.well-known", StaticFiles(directory=".well-known"), name="static")
+
+# Create a sub-application, in order to access just the query endpoint in an OpenAPI schema, found at http://0.0.0.0:8000/sub/openapi.json when the app is running locally
+sub_app = FastAPI(
+    title="Retrieval Plugin API",
+    description="A retrieval API for querying and filtering documents based on natural language queries and metadata",
+    version="1.0.0",
+    servers=[{"url": "https://your-app-url.com"}],
+    dependencies=[Depends(validate_token)],
+)
+app.mount("/sub", sub_app)
+
+
 @app.post(
     "/upsert-file",
     response_model=UpsertResponse,
 )
 async def upsert_file(
     file: UploadFile = File(...),
-    token: HTTPAuthorizationCredentials = Depends(validate_token),
 ):
     document = await get_document_from_file(file)
 
@@ -64,7 +64,6 @@ async def upsert_file(
 )
 async def upsert(
     request: UpsertRequest = Body(...),
-    token: HTTPAuthorizationCredentials = Depends(validate_token),
 ):
     try:
         ids = await datastore.upsert(request.documents)
@@ -80,7 +79,6 @@ async def upsert(
 )
 async def query_main(
     request: QueryRequest = Body(...),
-    token: HTTPAuthorizationCredentials = Depends(validate_token),
 ):
     try:
         results = await datastore.query(
@@ -100,7 +98,6 @@ async def query_main(
 )
 async def query(
     request: QueryRequest = Body(...),
-    token: HTTPAuthorizationCredentials = Depends(validate_token),
 ):
     try:
         results = await datastore.query(
@@ -118,7 +115,6 @@ async def query(
 )
 async def delete(
     request: DeleteRequest = Body(...),
-    token: HTTPAuthorizationCredentials = Depends(validate_token),
 ):
     if not (request.ids or request.filter or request.delete_all):
         raise HTTPException(
